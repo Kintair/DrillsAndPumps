49a07436616f5b138946247b54b67003a620419f
Alex Ludlow <a_ludlow@live.com>
2014-10-13 17:03:38 -0700

    Some cleanup

--- src/main/java/com/kintair/drillsandpumps/tileentities/TileEntitySmasher.java
+++ src/main/java/com/kintair/drillsandpumps/tileentities/TileEntitySmasher.java
@@ -1,15 +1,9 @@
 package com.kintair.drillsandpumps.tileentities;
 
-import com.kintair.drillsandpumps.DrillsAndPumps;
-
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTBase;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.world.World;
+
+import com.kintair.drillsandpumps.DrillsAndPumps;
 
 public class TileEntitySmasher extends TileEntity{
 	int tick = 0;
